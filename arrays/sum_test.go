54ec41b33537b48f984bcc743803c1f756606c51
greedy0110 <greedy0110@gmail.com>
2021-06-28 23:05:22 +0900

    add SumAll test

---
+++ arrays/sum_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+
+	assertEquals := func(t testing.TB, got, want int, numbers []int) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	}
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+		assertEquals(t, got, want, numbers)
+	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
